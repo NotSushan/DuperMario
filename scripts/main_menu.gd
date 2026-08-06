78a6c7cb015d4b01838e3acea493b5f9aa502986
Sushan <sushanhoraaa@gmail.com>
2026-08-06 21:24:25 +0545

    level connection and bug fixing

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -13,6 +13,9 @@ func _on_level_3_pressed() -> void:
 func _on_level_4_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level4.tscn")
 	
+func _on_level_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level5.tscn")
+	
 	
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/first_menu.tscn")

0dae455d7fc0e28c0fb32365e9cda7fd72f9204f
Sushan <sushanhoraaa@gmail.com>
2026-08-08 21:17:52 +0545

    error fixing

--- scripts/victory3.gd
+++ scripts/victory3.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 # Connected to your Next button signal
 func _on_next_pressed() -> void:
 	GameManager.points = 0
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")	
+	get_tree().change_scene_to_file("res://scenes/level4.tscn")	
 
 # Connected to your Play Again button signal
 func _on_play_again_pressed() -> void:
